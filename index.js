52fc660628562d3b332222eafb4dd7bd365520b9
staydevoooo <72087646+staydevoooo@users.noreply.github.com>
2022-12-18 03:43:04 -0500

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const Server = '[SERVER]';
 const crypto = require("crypto");
 
 // Start listening websocket on port
-const port = 80;
+const port = 7777;
 const wss = new WebSocket({ port: port }, console.log(Server, "Matchmaker started listening on port", port));
 
 wss.on('connection', async (ws) => {
