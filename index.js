8c58481a82841facea277f15c98facab656012b1
staydevoooo <72087646+staydevoooo@users.noreply.github.com>
2022-12-18 03:33:10 -0500

    Update index.js

--- index.js
+++ index.js
@@ -3,12 +3,12 @@ const Server = '[SERVER]';
 const crypto = require("crypto");
 
 // Start listening websocket on port
-const port = 443;
+const port = 80;
 const wss = new WebSocket({ port: port }, console.log(Server, "Matchmaker started listening on port", port));
 
 wss.on('connection', async (ws) => {
     if (ws.protocol.toLowerCase().includes("xmpp")) {
-        return;
+        return ws.close();
     }
 
     // create hashes
